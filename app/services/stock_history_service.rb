da09d8dd08cdfed9a4e06da3aa25f91b5a87ad18
Raphael-Maximowski <raphaelmxjv@gmail.com>
2025-09-24 18:10:39 -0300

    chore: remove history entity

--- app/services/stock_history_service.rb
+++
@@ -1,9 +0,0 @@
-class StockHistoryService 
-    def initialize(stock_history_repository = StockHistoryRepostory.new)
-        @stock_history_repository = stock_history_repository
-    end
-
-    def register_stock_manipulation()
-
-    end
-end
